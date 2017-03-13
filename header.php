72c77f1c8559e2ef537cd0b1f0773a0219e9faa3
Superzuzul <guillaume_azoulay@msn.com>
2017-03-13 13:55:45 +0100

    Nettoyage

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php $titre ?></title>
+	<meta charset="utf-8">
+	<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="./js/jquery-3.1.1.js"></script>
+	<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/main.css">
+</head>
+<body>
