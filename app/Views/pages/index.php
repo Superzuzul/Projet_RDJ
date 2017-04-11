2a774693696317d436be543331898f66be1a5e05
Superzuzul <guillaume_azoulay@msn.com>
2017-04-11 10:10:28 +0200

    modification de l'index

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -1,7 +1,37 @@
-<?php $this->layout('layout', ['title' => 'Accueil']) ?>
+<?php
 
-<?php $this->start('main_content') ?>
-	<h2>Let's code.</h2>
-	<p>Vous avez atteint la page d'accueil. Bravo.</p>
-	<p>Et maintenant, RTFM dans <strong><a href="../docs/tuto/" title="Documentation de W">docs/tuto</a></strong>.</p>
-<?php $this->stop('main_content') ?>
+$this->insert('partials/header',['title'=>"Accueil"]);
+
+?>
+
+<section class="articles">
+	<article id="sejourEte">
+		<h2>Séjours d'été</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis fuga harum obcaecati laudantium quidem, molestiae ducimus dicta repudiandae ad laboriosam.</p>
+		<p class="lienArticle"><a href="#">En savoir plus</a></p>
+	</article>
+	<article id="evenements">
+		<h2>Evenements à venir</h2>
+		<div>
+			<!-- UTILISER LE CONTROLLER POUR AFFICHER DYNAMIQUEMENT LA LISTE DES EVENEMENTS A VENIR
+			foreach ($evenements as $key => $evenement) {
+				echo "<div> Le ".dateFr($evenement["dateEvenement"])." : ".$evenement["nomEvenement"]." à ".$evenement['lieuEvenement']."</div>";
+			}
+			?> -->
+		</div>
+	</article>
+	<article id="projetAssociatif">
+		<h2>Notre projet associatif</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis fuga harum obcaecati laudantium quidem, molestiae ducimus dicta repudiandae ad laboriosam.</p>
+		<p class="lienArticle"><a href="#">En savoir plus</a></p>
+	</article>
+</section>
+
+
+
+
+<?php/*
+
+$this->insert('partials/footer');
+
+*/?>
