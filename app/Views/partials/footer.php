a8ac58ced768f548512df1b3771b1b8b2e762f7d
alexsmithh <hamelalexandresmith@gmail.com>
2017-04-12 09:57:37 +0200

    footer
    ajout du footer 12/04

---
+++ app/Views/partials/footer.php
@@ -0,0 +1,39 @@
+
+		<footer id="cont-footer">
+			<div class="container-fluid">
+			    <div class="row">    
+				
+					<div class="col-xs-3 text-left">
+					<h4 class="text-left">Plan du site :</h4>
+						<ul id="footer-plan-ul">
+							<li>Accueil</li>
+							<li>Séjours</li>
+							<li>Association</li>
+							<li>Aidez-nous</li>
+							<li>Adhérent</li>
+						</ul>
+					</div>
+					<div class="col-xs-6 text-center">
+						<h4 id="coord">Nos coordonnées :</h4>
+							<a id="adresse" href="https://goo.gl/maps/2LfEG8tXsvp" title="Ouvrir la carte interactive..." target="_blank"><i class="fa fa-2x fa-map-marker"></i> Centre d'hébergement Le PierLou, Le Bourg - 63980 ÉCHANDELYS</a>
+							<br/>
+							<div id="teleph"><i class="fa fa-1x fa-phone"></i> 06 80 26 05 74</div>
+					</div>
+					<div class="col-xs-3 text-center">
+						<h4 id="res-soc" class="text-center">Réseaux sociaux :</h4>
+						<a id="soc-yt" href="https://www.youtube.com/user/RevesDeJeux1984/" target="_blank"><i class="fa fa-3x fa-youtube"></i></a>
+						<a id="soc-fb" href="https://www.facebook.com/Revesdejeux" target="_blank"><i class="fa fa-3x fa-facebook-official" ></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="container-fluid">
+				<div id="pied" class="row">
+					<div class="col-xs-12 text-center">Copyright &copy; 2017 Rêves de Jeux | Tous droits réservés.</div>
+					<div class="col-xs-12 text-center">- Mentions légales -</div>
+				</div>	
+			</div>
+		</footer>
+	
+	<script type="text/javascript" src="../js/main.js"></script>
+	</body>
+</html>
