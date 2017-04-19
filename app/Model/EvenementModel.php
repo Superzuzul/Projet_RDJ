1e5acd2ca94de29404e715cd88dc79e64bbcfa3b
Superzuzul <guillaume_azoulay@msn.com>
2017-04-19 18:22:24 +0200

    modification index et le model evenement + ajout slider +ajout glyphicon

--- app/Model/EvenementModel.php
+++ app/Model/EvenementModel.php
@@ -5,7 +5,16 @@ use \W\Model\Model;
 
 class EvenementModel extends Model{
     
-    
+    public function find5()
+	{
+		
+		$sql = "SELECT id, nomEvenement, dateEvenement, lieuEvenement FROM evenement WHERE dateEvenement >= NOW() LIMIT 5";
+		$sth = $this->dbh->prepare($sql);
+		
+		$sth->execute();
+
+		return $sth->fetchAll();
+	}
     
     
 }
--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -1,28 +1,84 @@
 <?php
 $this->insert('partials/header',['titre'=>"Rêves de Jeux Classique, colonie de vacance pour les 12/17 ans, acitivités Jeux de Rôles, Jeux de Sociétés, Jeux vidéos...", 'description'=>"Rêves de Jeux propose des colonies de vacances pour les 12/17 ans pendant l'été, spécialisés le jeu sous toutes ses formes (Jeux de sociétés, Jeux de rôles, Jeux vidéos, Grandeur Natures, activités extérieurs)"]);
 ?>
+<section>
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+	  <!-- Indicators -->
+	  <ol class="carousel-indicators">
+	    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+	  </ol>
 
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner" role="listbox">
+	    <div class="item active">
+	      <img src="<?php echo $this->assetUrl("/img/slider/slider1.jpg") ?>" alt="image1">
+	      <div class="carousel-caption">
+	        ...
+	      </div>
+	    </div>
+	    <div class="item">
+	      <img src="<?php echo $this->assetUrl("/img/slider/slider2.jpg") ?>" alt="...">
+	      <div class="carousel-caption">
+	        ...
+	      </div>
+	    </div>
+	    <div class="item">
+	      <img src="<?php echo $this->assetUrl("/img/slider/slider3.jpg") ?>" alt="...">
+	      <div class="carousel-caption">
+	        ...
+	      </div>
+	    </div>
+	    ...
+	  </div>
+
+	  <!-- Controls -->
+	  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+	    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+	    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+</section>
 <section class="articles">
-	<article id="sejourEte">
-		<h2>Séjours d'été</h2>
-		<p class="positionTexte">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis fuga harum obcaecati laudantium quidem, molestiae ducimus dicta repudiandae ad laboriosam.</p>
-		<p class="lienArticle"><a href="#">En savoir plus</a></p>
-	</article>
-	<article id="evenements">
-		<h2>Evenements à venir</h2>
-		<div>
-			<!-- UTILISER LE CONTROLLER POUR AFFICHER DYNAMIQUEMENT LA LISTE DES EVENEMENTS A VENIR
-			foreach ($evenements as $key => $evenement) {
-				echo "<div> Le ".dateFr($evenement["dateEvenement"])." : ".$evenement["nomEvenement"]." à ".$evenement['lieuEvenement']."</div>";
-			}
-			?> -->
+	<div class="row">
+		<div class="col-xs-12 col-md-4">
+			<h2>Séjours d'été</h2>
+			<p>
+				Nos "Geek-Anim" très spécialisés vous proposent l'immersion dans le monde des jeux sous toute ses facettes : de société, d'aventure, de rôle, de simulation, de coopération, avec du Grandeur Nature, des jeux en réseaux, des événementiels thémathisés...
+			</p>
+			<p>
+				<a href="<?= $this->url('default_rdj_classique') ?>">En savoir plus...</a>
+			</p>
 		</div>
-	</article>
-	<article id="projetAssociatif">
-		<h2>Notre projet associatif</h2>
-		<p class="positionTexte">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis fuga harum obcaecati laudantium quidem, molestiae ducimus dicta repudiandae ad laboriosam.</p>
-		<p class="lienArticle"><a href="#">En savoir plus</a></p>
-	</article>
+		<div class="col-xs-12 col-md-4">
+			<h2>Notre projet éducatif</h2>
+			<p>
+				Les responsables de nos différentes activités et les équipes d’encadrement de tous nos séjours se réfèrent, dans toutes leurs actions, à un projet pédagogique qui est l’application logique et directe du projet éducatif de l’association « RÊVES DE JEUX », basé sur l’objet de ses statuts : Cette association a pour but la rencontre et la communication des personnes (enfants, adolescents, adultes)...
+			</p>
+			<p>
+				<a href="<?= $this->url('default_projet_educatif') ?>">En savoir plus...</a>
+			</p>
+		</div>
+		<div class="col-xs-12 col-md-4">
+			<h2>Evenements à venir</h2>
+			<p>
+				<?php
+				$objetEvenementModel=new \Model\EvenementModel;
+				$evenements=$objetEvenementModel->find5();
+				foreach ($evenements as $key => $evenement) {
+					echo "<div> Le ".$this->dateFr($evenement["dateEvenement"])." : ".$evenement["nomEvenement"]." à ".$evenement['lieuEvenement']."</div>";
+				}
+
+
+				?>
+			</p>
+		</div>
+	</div>
 </section>
 
 <?php
